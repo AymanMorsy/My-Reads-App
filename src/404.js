f94ae9c16426794c622838bee06fad54372f425d
AymanMorsy <aymanmorsy.eg@gmail.com>
2018-08-14 19:29:32 +0200

    Update 404.js

--- src/404.js
+++ src/404.js
@@ -5,12 +5,10 @@ import Err404 from './404.png';
 const Error404 = ()=>{
     return(
         <div className ='error-container'>
-        <h3 className ='error-title'>This Page Not Found</h3>
-        <img src={Err404} alt="page Not found 404" className='error_404'/>
+            <h3 className ='error-title'>This Page Not Found</h3>
+            <img src={Err404} alt="page Not found 404" className='error_404'/>
         </div>
     )
 }
 
 export default Error404
-
-// alt="Paris" style="width:50%;"
